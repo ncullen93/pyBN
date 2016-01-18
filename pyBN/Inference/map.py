b686600c45e90ee5fb9ae4d2a630f7a1191fb07f
ncullen93 <nicholas.c.cullen.th@dartmouth.edu>
2016-01-18 11:18:52 -0500

    major

---
+++ pyBN/Inference/map.py
@@ -0,0 +1,13 @@
+"""
+******************************
+Maximum A Posteriori Inference
+******************************
+
+Perform MAP inference over a BayesNet object.
+
+We support exact/approx inference, with or without evidence.
+
+
+"""
+
+__author__ = """N. Cullen <ncullen.th@dartmouth.edu>"""
