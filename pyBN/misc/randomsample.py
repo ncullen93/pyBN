f87914c86097cb2fc0cde73e127c3a1039f80241
ncullen93 <nicholas.c.cullen.th@dartmouth.edu>
2016-01-21 23:39:45 -0500

    misc

--- pyBN/misc/randomsample.py
+++ pyBN/misc/randomsample.py
@@ -13,13 +13,16 @@ __author__ = """Nicholas Cullen <ncullen.th@dartmouth.edu>"""
 from pyBN.classes.factor import Factor
 import networkx as nx
 
-def random_sample(self, n=1, evidence={}):
+def random_sample(bn, n=100, evidence={}):
     """
     Take a random sample of "n" observations from a
-    BayesNet object.
+    BayesNet object. This is essentially just a repeated
+    forward sample algorithm that returns every sample.
 
     Parameters
     ----------
+    *bn* : a BayesNet object from which to sample
+
     *n* : an integer
         The number of observations to take
 
@@ -28,23 +31,19 @@ def random_sample(self, n=1, evidence={}):
 
     Returns
     -------
-    *sample* : a numpy array
-        The generated random sample
+    *sample_dict* : a list of samples, where each sample
+        is a dictionary containing each variable as a key
+        and its sampled value as the dictionary value
 
     Notes
     -----
-    - This is a prime candidate for numba.
-    - Does not currently work.
+    - Not tested
 
     """
     sample_dict = []
     weight_list = np.ones(n)
 
-    G = self.BN.get_networkx()
-    rv_order = nx.topological_sort(G)
-
-    #factor_dict = dict([(var,FastFactor(self.BN, var)) for var in self.BN.V])
-    parent_dict = dict([(var, G.predecessors(var)) for var in self.BN.V])
+    parent_dict = dict([(var, bn.data[var]['parents']) for var in bn.V])
 
     for i in range(n):
         if i % (n/float(10)) == 0:
@@ -52,23 +51,23 @@ def random_sample(self, n=1, evidence={}):
                 print 'Sample: ' , i
         new_sample = {}
         for rv in rv_order:
-            f = Factor(self.BN,rv)
+            f = Factor(bn,rv)
             # reduce_factor by parent samples
             for p in parent_dict[rv]:
                 f.reduce_factor(p,new_sample[p])
             # if rv in evidence, choose that value and weight
             if rv in evidence.keys():
                 chosen_val = evidence[rv]
-                weight_list[i] *= f.cpt[self.BN.data[rv]['vals'].index(evidence[rv])]
+                weight_list[i] *= f.cpt[bn.data[rv]['vals'].index(evidence[rv])]
             # if rv not in evidence, sample as usual
             else:
-                choice_vals = self.BN.data[rv]['vals']
+                choice_vals = bn.data[rv]['vals']
                 choice_probs = f.cpt
                 chosen_val = np.random.choice(choice_vals, p=choice_probs)
                 
             new_sample[rv] = chosen_val
         sample_dict.append(new_sample)
 
-    self.sample_dict = sample_dict
+    return sample_dict
 
 
