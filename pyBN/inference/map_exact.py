33dc85ef1b60e58310ca9d5b43b13b9f3393a175
ncullen93 <nicholas.c.cullen.th@dartmouth.edu>
2016-02-02 10:49:14 -0500

    map

--- pyBN/inference/map_exact.py
+++ pyBN/inference/map_exact.py
@@ -13,7 +13,7 @@ an argument to that function.
 Exact MAP Inference Algorithms
 ------------------------------
 
-	- Max-Sum Variable Elimination
+    - Max-Sum Variable Elimination
 
 
 References
@@ -31,179 +31,159 @@ from pyBN.classes.factor import Factor
 
 
 def map_ve_e(bn,
-			target=None,
-			evidence={}):
-	"""
-	Perform Max-Sum Variable Elimination over a BayesNet object
-	for exact maximum a posteriori inference.
+            target=None,
+            evidence={}):
+    """
+    Perform Max-Sum Variable Elimination over a BayesNet object
+    for exact maximum a posteriori inference.
 
-	Parameters
-	----------
-
-
-	Returns
-	-------
+    Parameters
+    ----------
 
 
-	Effects
-	-------
+    Returns
+    -------
 
 
-	Notes
-	-----
+    Effects
+    -------
 
-	"""
-	assert (target not in evidence.keys()), 'Target is already in Evidence..'
 
-	temp_F = [Factor(bn,var) for var in bn.nodes()]
+    Notes
+    -----
 
-	map_list = []
+    """
+    assert (target not in evidence.keys()), 'Target is already in Evidence..'
 
-	#### ORDER HANDLING ####
-	order = copy.copy(list(bn.nodes()))
-	order.remove(target)
-	
-	##### EVIDENCE #####
-	#if len(evidence)>0:
-	#	assert isinstance(evidence, dict), 'Evidence must be Dictionary'
-	#	temp=[]
-	#	for f in temp_F:
-	#		for rv, val in evidence.items():
-	#			#if len(f.scope)>1 or rv not in f.scope:
-	#				#temp.append(f)
-	#			if rv in f.scope:
-	#				f.reduce_factor(rv,val)
-	#		order.remove(rv)
-	#		temp.append(f)
-	#	temp_F=temp
+    _phi = [Factor(bn,var) for var in bn.nodes()]
 
-	if len(evidence)>0:
-		assert isinstance(evidence, dict), 'Evidence must be Dictionary'
-		temp=[]
-		for obs in evidence.items():
-			for f in temp_F:
-				if len(f.scope)>1 or obs[0] not in f.scope:
-					temp.append(f)
-				if obs[0] in f.scope:
-					f.reduce_factor(obs[0],obs[1])
-			order.remove(obs[0])
-		temp_F=temp
+    #### ORDER HANDLING ####
+    order = copy.copy(list(bn.nodes()))
+    order.remove(target)
+    
+    #### EVIDENCE PROCESSING ####
+    for E, e in evidence.items():
+        for phi in _phi:
+            if E in phi.scope:
+                phi.reduce_factor(E,e)
+        order.remove(E)
 
-	
-	#### ALGORITHM ####
-	for var in order:
-		relevant_factors = [f for f in temp_F if var in f.scope]
-		irrelevant_factors = [f for f in temp_F if var not in f.scope]
+    #### ALGORITHM ####
+    for var in order:
+        phi_bold, traceback[var] = max_prod_eliminate_var(phi_bold, var)
+    
+    x_star = traceback_map(traceback)
+    
+    return bn.values(target)[np.argmax(marginal)]
+    
+        
 
-		# mutliply all relevant factors
-		fmerge = relevant_factors[0]
-		for i in range(1,len(relevant_factors)):
-			fmerge.multiply_factor(relevant_factors[i])
-		
-		fmerge.maxout_var(var)
+def max_prod_eliminate_var(_phi, Z):
+    relevant_factors = [f for f in temp_F if var in f.scope]
+    irrelevant_factors = [f for f in temp_F if var not in f.scope]
 
-		irrelevant_factors.append(fmerge) # add sum-prod factor back in
-		temp_F = irrelevant_factors
-	
-	marginal = temp_F[0]
-	# multiply final factors in factor_list
-	if len(temp_F) > 1:
-		for i in range(1,len(temp_F)):
-			marginal.multiply_factor(temp_F[i])
-	marginal.normalize()
-	
-	return bn.values(target)[np.argmax(marginal)]
+    # mutliply all relevant factors
+    psi = relevant_factors[0]
+    for i in range(1,len(psi)):
+        psi.multiply_factor(relevant_factors[i])
+    
+    _psi = deepcopy(psi)
+    psi.maxout_var(var)
+    irrelevant_factors.append(psi) # add sum-prod factor back in
+    
+    return irrelevant_factors, _psi
+    
 
 def map_opt_e(bn, evidence={}):
-	"""
-	Solve MAP Inference as a dynamic programming
-	problem, where the solution is built up from
-	the leaf nodes by solving subproblems of
-	maximal probability rv assignments at each node
+    """
+    Solve MAP Inference as a dynamic programming
+    problem, where the solution is built up from
+    the leaf nodes by solving subproblems of
+    maximal probability rv assignments at each node
 
-	Arguments
-	---------
-	*bn* : a BayesNet object
+    Arguments
+    ---------
+    *bn* : a BayesNet object
 
-	*evidence* : a dictionary, where
-		key = rv and value = rv's value
+    *evidence* : a dictionary, where
+        key = rv and value = rv's value
 
-	Returns
-	-------
-	*sol* : a dictionary, where
-		key = rv and value = maximal assignment
+    Returns
+    -------
+    *sol* : a dictionary, where
+        key = rv and value = maximal assignment
 
-	Effects
-	-------
-	None
+    Effects
+    -------
+    None
 
-	Notes
-	-----
-	decision variables:
-		variable for each set of values in a cpt
-	objective:
-		minimize sum of negative log probabilities
-	constraints:
-		- sum for all variables in a cpt must be 1
-		- intersection of variables between cpt must agree
-	"""
-	if evidence:
-			assert isinstance(evidence, dict), 'Evidence must be in dictionary form'
+    Notes
+    -----
+    decision variables:
+        variable for each set of values in a cpt
+    objective:
+        minimize sum of negative log probabilities
+    constraints:
+        - sum for all variables in a cpt must be 1
+        - intersection of variables between cpt must agree
+    """
+    if evidence:
+            assert isinstance(evidence, dict), 'Evidence must be in dictionary form'
 
-	model = LpProblem("MAP Inference",LpMinimize)
-	var_dict = {} # maps variable string name to actual variable
-	var_list = []
-	for node in bn.nodes():
-		for cpt_idx,cpt_val in enumerate(bn.cpt(node)):
-			new_var = LpVariable(str(str(node)+'-'+str(cpt_idx)),0,1,LpInteger)
-			var_list.append(new_var)
-			var_dict[str(str(node)+'-'+str(cpt_idx))] = new_var
-			node_var_dict[node].append(new_var)
-			weight_list.append(-np.log(cpt_val))
+    model = LpProblem("MAP Inference",LpMinimize)
+    var_dict = {} # maps variable string name to actual variable
+    var_list = []
+    for node in bn.nodes():
+        for cpt_idx,cpt_val in enumerate(bn.cpt(node)):
+            new_var = LpVariable(str(str(node)+'-'+str(cpt_idx)),0,1,LpInteger)
+            var_list.append(new_var)
+            var_dict[str(str(node)+'-'+str(cpt_idx))] = new_var
+            node_var_dict[node].append(new_var)
+            weight_list.append(-np.log(cpt_val))
 
-	model += np.dot(weight_list,var_list) # minimizes -1*var*probability
+    model += np.dot(weight_list,var_list) # minimizes -1*var*probability
 
-	# constraint set 1
-	# exactly one choice from each factor
-	k = 0
-	for rv in bn.nodes():
-		cell = node_var_dict[rv]
-		model += np.sum(cell) == 1, "Factor Sum Constraint" + str(k)
-		k+=1
+    # constraint set 1
+    # exactly one choice from each factor
+    k = 0
+    for rv in bn.nodes():
+        cell = node_var_dict[rv]
+        model += np.sum(cell) == 1, "Factor Sum Constraint" + str(k)
+        k+=1
 
-	factor_dict = dict([(rv, Factor(bn,rv)) for rv in bn.nodes()])
-	# constraint set 2
-	# intersection of factors must agree
-	for rv1 in bn.nodes():
-		for rv2 in bn.children(rv):
-			f_rv1 = factor_dict[rv1]
-			f_rv2 = factor_dict[rv2]
-			# get their intersection
-			#intersection_vars = set(bn.scope(rv1)) & set(bn.scope(rv2))
-			#for var in intersection_vars:
-			for value in bn.values(rv1):
-				# get indices of var-value in f_rv1
-				f_rv1_indices = f_rv1.value_indices(rv1,value)
-				# get indices of var-value in f_rv2
-				f_rv2_indices = f_rv2.value_indices(rv2,value)
-				# get the associated model variables for f_rv1
-				f_rv1_vars = [var_dict[str(str(rv1)+'-'+str(idx))] for idx in f_rv1_indices]
-				f_rv2_vars = [var_dict[str(str(rv2)+'-'+str(idx))] for idx in f_rv2_indices]
-				# create constraint that they must sum to the same thing
-				model+= np.sum(f_rv1_vars)-np.sum(f_rv2_vars) == 0, 'Factor Agreement Constraint' + str(k)
-			k+=1
+    factor_dict = dict([(rv, Factor(bn,rv)) for rv in bn.nodes()])
+    # constraint set 2
+    # intersection of factors must agree
+    for rv1 in bn.nodes():
+        for rv2 in bn.children(rv):
+            f_rv1 = factor_dict[rv1]
+            f_rv2 = factor_dict[rv2]
+            # get their intersection
+            #intersection_vars = set(bn.scope(rv1)) & set(bn.scope(rv2))
+            #for var in intersection_vars:
+            for value in bn.values(rv1):
+                # get indices of var-value in f_rv1
+                f_rv1_indices = f_rv1.value_indices(rv1,value)
+                # get indices of var-value in f_rv2
+                f_rv2_indices = f_rv2.value_indices(rv2,value)
+                # get the associated model variables for f_rv1
+                f_rv1_vars = [var_dict[str(str(rv1)+'-'+str(idx))] for idx in f_rv1_indices]
+                f_rv2_vars = [var_dict[str(str(rv2)+'-'+str(idx))] for idx in f_rv2_indices]
+                # create constraint that they must sum to the same thing
+                model+= np.sum(f_rv1_vars)-np.sum(f_rv2_vars) == 0, 'Factor Agreement Constraint' + str(k)
+            k+=1
 
 
-	#add constraint set 3
-	#all evidence variables set = 1
-			
-	model.solve()
-	max_inference = dict([(v.name,v.varValue) for v in model.variables() if v.varValue == 1])
-   	return max_inference
+    #add constraint set 3
+    #all evidence variables set = 1
+            
+    model.solve()
+    max_inference = dict([(v.name,v.varValue) for v in model.variables() if v.varValue == 1])
+       return max_inference
 
-	
+    
 
-	
+    
 
 
 
